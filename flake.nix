e5e6701909e74aaef2d8f156a3d849994616617c
tyleranton <tylerdotdev@gmail.com>
2025-09-11 09:55:48 -0700

    cleanup and modify action workflow

--- flake.nix
+++ flake.nix
@@ -23,20 +23,6 @@
             rust-bin.stable.latest.default
           ];
         };
-
-        devShells.windows = with pkgs; mkShell {
-          buildInputs = [
-            pkg-config
-            (rust-bin.stable.latest.default.override {
-              targets = [ "x86_64-pc-windows-gnu" ];
-            })
-            pkgsCross.mingwW64.stdenv.cc
-          ];
-
-          CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
-          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "${pkgsCross.mingwW64.stdenv.cc.targetPrefix}gcc";
-          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_AR = "${pkgsCross.mingwW64.stdenv.cc.targetPrefix}ar";
-        };
       }
     );
 }
